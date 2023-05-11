0dc63a3e8f6beddb63c9e751f3f2963ff1015113
Juebele <eronuebele@gmail.com>
2023-05-10 21:01:01 -0400

    added schema and seeds files

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO tags (tag_name) VALUES ("TAG 1"), ("TAG 2"), ("TAG 3");
+
+INSERT INTO categories (category_name) VALUES ("Clothing"), ("Electronics"), ("Decor");
+
+INSERT INTO products (product_name, price, stock, category_id) VALUES ("Shirt", 8, 15, 1), ("Speaker", 60, 10, 2), ("Lamp", 35, 12, 3);
